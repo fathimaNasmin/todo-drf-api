364e4db57835d26b5617b8258ed0fd812fc478ae
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-16 15:53:07 -0400

    write test and implement user profile for get and patch method

--- app/todo/urls.py
+++ app/todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import UserLoginAPIView
-from todo.views import UserRegisterAPIView, CreateTokenView
+from todo.views import UserRegisterAPIView, CreateTokenView, UserProfileView
 # from .views import UserLogoutAPIView
 
 app_name = 'todo'
@@ -8,4 +8,5 @@ app_name = 'todo'
 urlpatterns = [
     path("user/register/", UserRegisterAPIView.as_view(), name="register"),
     path("user/login/", CreateTokenView.as_view(), name="login"),
+    path("user/profile/", UserProfileView.as_view(), name="profile"),
 ]
--- app/todo/views.py
+++ app/todo/views.py
@@ -1,6 +1,6 @@
 """Views for api end points"""
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -16,3 +16,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new token for the authenticated user."""
     serializer_class = serializers.AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
+    
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    """View class to get and update the user."""
+    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+    
+    def get_object(self):
+        """Retrieve and update the user."""
+        return self.request.user
