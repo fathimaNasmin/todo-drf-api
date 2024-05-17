eb0d0ebfd494c6ad878462a6590ed7ff332d7ee4
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-17 10:24:30 -0400

    add id to the user serializer

--- app/todo/serializers.py
+++ app/todo/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = get_user_model()
-        fields = ['name', 'email', 'password', 'preferred_theme']
+        fields = ['id', 'name', 'email', 'password', 'preferred_theme']
         extra_kwargs = {
             'password': {
                 'write_only': True,
