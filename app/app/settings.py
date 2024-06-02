993b21913ecbefdd6cb265c29ae5e218480ec361
Fathima nasmin <fathimanesmi@gmail.com>
2024-06-02 07:17:48 -0400

    settings change

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'young-brook-24556.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '*']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
